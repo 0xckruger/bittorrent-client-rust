c2011f7ede37b8c341acc4f1d3972b9ced2fe8f4
ckruger097 <ckruger4@gmu.edu>
2023-11-03 20:39:44 -0500

    refactored

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,83 @@
 use serde_json;
 use std::env;
-use serde_json::{Number, Value};
+use std::iter::Peekable;
+use std::str::Chars;
+use serde_json::{Value};
 use serde_json::Value::Array;
 
-// Available if you need it!
-// use serde_bencode
-
-fn decode_bencoded_structure(encoded_value: &str) -> Value {
-
-    //println!("decoding: {}", encoded_value);
-    if encoded_value.chars().next().unwrap() == 'l' {
-        if encoded_value.chars().nth(1).unwrap() == 'l' {
-            return Array(vec![decode_bencoded_structure(&encoded_value[1..encoded_value.len()-1])]);
-        }
-
-        if encoded_value == "le" {
-            return Array(vec![]);
-        }
-
-        let mut list: Value = Array(vec![]);
-        let mut i = 1;
-        while i < encoded_value.len() - 1 {
-            let value = decode_bencoded_structure(&encoded_value[i..encoded_value.len()-1]);
-            if value.is_number() {
-                //println!("val: {}", value);
-                let string_length = value.to_string().len();
-                i += string_length;
-                i += 2;
-            } else {
-                //println!("val: {}", value);
-                let string_length = value.to_string().len() - 2;
-                i += string_length;
-                i += string_length.to_string().len();
-                //println!("{} + {}", string_length, string_length.to_string().len());
-                i += 1;
-            }
-            //println!("got {} @ {}", value, i);
-            list.as_array_mut().expect("Not an array").push(value);
-            }
-        return list;
-        }
-    decode_bencoded_value(encoded_value)
+fn decode_bencoded_structure_new(encoded_value: &str) -> Result<serde_json::Value, &'static str> {
+    let mut chars = encoded_value.chars().peekable();
+    parse_bencoded_values(&mut chars)
 }
 
-#[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> Value {
-    // If encoded_value starts with a digit, it's a string
-    //println!("trying to decode value: {}", encoded_value);
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return Value::String(string.to_string())
+fn parse_bencoded_values(chars: &mut Peekable<Chars>) -> Result<Value, &'static str> {
+    //println!("matching: {:?}", chars);
+    let mut chars_peekable = chars.clone().peekable();
+    match chars_peekable.peek() {
+        Some('i') => parse_bencoded_number(chars),
+        Some('l') => parse_bencoded_list(chars),
+        Some(c) if c.is_digit(10) => parse_bencoded_string(chars, c.to_digit(10).unwrap() as usize),
+        _ => Err("Unable to parse bencoded values"),
     }
-    // if encoded_value starts with an i and ends in e it's a number
-    else if encoded_value.chars().next().unwrap() == 'i' {
-        let e_index = encoded_value.find('e').unwrap();
-        let integer_string = &encoded_value[1..e_index];
-        let integer = integer_string.parse::<i64>().unwrap();
-        return Value::Number(Number::from(integer))
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
+}
+
+fn parse_bencoded_number(chars: &mut Peekable<Chars>) -> Result<Value, &'static str> {
+    let mut number = String::new();
+    let mut is_float = false;
+
+    chars.next(); // i
+
+    while let Some(c) = chars.next() {
+        match c {
+            '.' => {
+                is_float = true;
+                number.push(c);
+            }
+            'e' => {
+                if is_float {
+                    if let Ok(float_val) = number.parse::<f64>() {
+                        return Ok(serde_json::Value::Number(serde_json::Number::from_f64(float_val).unwrap()));
+                    }
+                } else {
+                    if let Ok(int_val) = number.parse::<i64>() {
+                        return Ok(serde_json::Value::Number(serde_json::Number::from(int_val)));
+                    }
+                }
+                return Err("Invalid number");
+            }
+            _ => number.push(c),
+        }
     }
+
+    Err("Unclosed number")
+}
+
+fn parse_bencoded_string(chars: &mut Peekable<Chars>, length: usize) -> Result<Value, &'static str> {
+    //println!("{:?}", chars);
+    chars.next(); // number
+    chars.next(); // :
+
+    let data: String = chars.take(length).collect();
+    Ok(Value::String(data))
+}
+
+fn parse_bencoded_list(chars: &mut Peekable<Chars>) -> Result<Value, &'static str> {
+    let mut list = Vec::new();
+    chars.next(); // l
+    while let Some(c) = chars.peek() {
+        match c {
+            'e' => {
+                chars.next(); // e
+                return Ok(Array(list))
+            }
+            _ => {
+                //println!("pushing {}", c);
+                list.push(parse_bencoded_values(chars)?);
+                //println!("{:?} - {:?}", list, chars);
+            }
+        }
+    }
+    Err("Unclosed list")
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
@@ -77,8 +91,8 @@ fn main() {
 
         // Uncomment this block to pass the first stage
         let encoded_value = &args[2];
-        let decoded_value = decode_bencoded_structure(encoded_value);
-        println!("{}", decoded_value.to_string());
+        let decoded_value = decode_bencoded_structure_new(encoded_value);
+        println!("{}", decoded_value.unwrap().to_string());
     } else {
         println!("unknown command: {}", args[1])
     }
